41da8ba5138b488f190c40d19f124701f2781bc2
tan pham <tanhd.nuce@gmail.com>
2018-06-11 16:58:15 +0700

    change

---
+++ src/main/java/practice/spring/com/entity/User.java
@@ -0,0 +1,118 @@
+package practice.spring.com.entity;
+
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import practice.spring.com.validator.Phone;
+
+public class User {
+	
+public User() {}
+	
+public User(String id, String name, String email, Date dateOfBirth, String phoneNumber) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.dateOfBirth = dateOfBirth;
+		this.phoneNumber = phoneNumber;
+	}
+
+@NotNull(message = "Id may not be null")
+private String id;
+
+@NotBlank
+@Length(min = 5, max = 10)
+private String name;
+
+@NotBlank
+@Email
+private String email;
+
+@NotNull
+@DateTimeFormat(pattern = "dd/MM/yyyy")
+@Past
+private Date dateOfBirth;
+
+@Phone(message = "Phone Number is invalid")
+private String phoneNumber;
+
+/**
+ * @return the id
+ */
+public String getId() {
+	return id;
+}
+
+/**
+ * @param id the id to set
+ */
+public void setId(String id) {
+	this.id = id;
+}
+
+/**
+ * @return the name
+ */
+public String getName() {
+	return name;
+}
+
+/**
+ * @param name the name to set
+ */
+public void setName(String name) {
+	this.name = name;
+}
+
+/**
+ * @return the email
+ */
+public String getEmail() {
+	return email;
+}
+
+/**
+ * @param email the email to set
+ */
+public void setEmail(String email) {
+	this.email = email;
+}
+
+/**
+ * @return the dateOfBirth
+ */
+public Date getDateOfBirth() {
+	return dateOfBirth;
+}
+
+/**
+ * @param dateOfBirth the dateOfBirth to set
+ */
+public void setDateOfBirth(Date dateOfBirth) {
+	this.dateOfBirth = dateOfBirth;
+}
+
+/**
+ * @return the phoneNumber
+ */
+public String getPhoneNumber() {
+	return phoneNumber;
+}
+
+/**
+ * @param phoneNumber the phoneNumber to set
+ */
+public void setPhoneNumber(String phoneNumber) {
+	this.phoneNumber = phoneNumber;
+}
+
+
+}
--- src/main/webapp/WEB-INF/views/jsp/index.jsp
+++ src/main/webapp/WEB-INF/views/jsp/index.jsp
@@ -1,4 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+in<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -9,50 +9,8 @@
 </head>
 <body>
 	<h1>Index</h1>
-	<a href="home">go home</a><br>
-	<a href="test1/11">/test1/1</a>
+	<a href="index">go home</a><br>
+	<a href="addUser">add User</a>
   <br />
-  <a href="test2/222/kai">/test2/2/kai</a>
-  <br />
-  <fieldset>
-    <legend>/test3?id=&name=</legend>
-      Id: <input type="number" name="id" id="id"/> <br /> 
-      Name: <input type="text" name="name" id="name"/> <br /> 
-      <input type="submit" value="submit" id ="submit"/>
-  </fieldset>
-<!--         <div id="server-results"></div> -->
-<p id="test1">This is a paragraph.</p>
-  
-  <script type="text/javascript">
-	$("#submit").on("click", function(event) {
-		event.preventDefault();
-		var id = $("#id").val();
-	  	var name = $("#name").val();
-	  	var data_form = {};
-	  	data_form["id"] = id;
-	  	data_form["name"] = name;
-	  	$.ajax({
-	  		type: "POST",
-	  		url: "${pageContext.request.contextPath}/getJson",
-			data: {data_form: JSON.stringify(data_form), id: id, name: name},
-// 			contentType: 'application/json; charset=UTF-8',
-// 			dataType: 'json',
-	  		success : function(data) {
-	              alert(JSON.stringify(data));
-	              var res = JSON.stringify(data);
-// 	              $("#server-results").innerHTML =  "nội dung result" + data;
-					if(res != null || res != undefined){
-						$("#test1").text(res);
-					} else {
-						$("#test1").text("xxx");
-					}
-					$("#test1").css("color","red");
-	            },
-            error: function(){                     
-                alert('Error while request..');
-            }
-	  	});
-	});
-  </script>
 </body>
 </html>
